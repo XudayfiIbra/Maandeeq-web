37da73b20b77f26ba6d005988ce1db91ceac2317
xudayfi <xudayfiibraahim8@gmail.com>
2024-06-06 10:52:29 +0300

    edit cors

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5#8p=w$n2y!wmk2&@$$treemddej&c-g_8zo25ge3&m$zynzd3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
